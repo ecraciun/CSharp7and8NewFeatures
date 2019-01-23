60628c823dba084cbfa620ce2fd8e7b37af3f48f
Emil Craciun <emil.craciun@live.com>
2019-01-23 16:06:30 +0200

    More work on C# 7.0

--- src/CS7.0/Program.cs
+++ src/CS7.0/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace CS7._0
 {
@@ -7,6 +8,14 @@ namespace CS7._0
         static void Main(string[] args)
         {
             OutVariables();
+
+            Tuples();
+
+            Discards();
+
+            PatternMatching();
+
+            ThrowExpressions();
 
             Console.WriteLine("Press any key to end...");
             Console.ReadKey();
@@ -83,6 +92,13 @@ namespace CS7._0
         {
             public int L { get; }
             public int H { get; }
+            public int Area
+            {
+                get
+                {
+                    return L * H;
+                }
+            }
 
             public Rectangle(int l, int h)
             {
@@ -95,7 +111,123 @@ namespace CS7._0
                 h = H;
                 l = L;
             }
+
+            public (int Length, int Height, int Area) GetRectangleInfo1()
+            {
+                return (L, H, Area);
+            }
+
+            public void GetRectangleInfo2(out int Length, out int Height, out int RectangleArea)
+            {
+                Length = L;
+                RectangleArea = Area;
+                Height = H;
+            }
         }
+
+        #endregion
+
+        #region Discards
+
+        private static void Discards()
+        {
+            // Deconstruction
+            var r = new Rectangle(5, 10);
+            var (_, _, area) = r.GetRectangleInfo1();
+            (int length, _) = r;
+
+            // Out variables
+            r.GetRectangleInfo2(out _, out _, out var a);
+
+            // Standalone
+            _ = Task.Run(() =>
+           {
+               Console.WriteLine("Hello from a task");
+           });
+
+            IsItARectangle(r);
+            IsItARectangle(null);
+            IsItARectangle(2);
+        }
+
+        private static void IsItARectangle(object obj)
+        {
+            if(obj is Rectangle)
+            {
+                Console.WriteLine("Yup");
+            }
+            else if (obj is null)
+            {
+                Console.WriteLine("It's null");
+            }
+            else
+            {
+                Console.WriteLine("Idk what it is, but it's not null");
+            }
+        }
+
+        #endregion
+
+        #region Pattern matching
+
+        private static void PatternMatching()
+        {
+
+        }
+
+        #endregion
+
+        #region Ref locals and returns
+
+
+
+        #endregion
+
+        #region Local functions
+
+
+
+        #endregion
+
+        #region More expression-bodied members
+
+
+
+        #endregion
+
+        #region Throw expressions
+
+        private static void ThrowExpressions()
+        {
+            var result = GiveMeSomethingRandom() ?? throw new ArgumentNullException("Well it's not exactly an argument, but you get it");
+        }
+
+        private static object GiveMeSomethingRandom()
+        {
+            Random r = new Random();
+            var shouldReturnNull = r.Next(100) % 2 == 0 ? true : false;
+            return shouldReturnNull ? null : new object();
+        }
+
+        #endregion
+
+        #region Generalized async return types
+
+
+
+        #endregion
+
+        #region Numeric literal syntax improvements
+
+        public const int One = 0b0001;
+
+        public const int Sixteen = 0b0001_0000;
+
+        public const long BillionsAndBillions = 100_000_000_000;
+
+        public const double AvogadroConstant = 6.022_140_857_747_474e23;
+
+        public const decimal GoldenRatio = 1.618_033_988_749_894_848_204_586_834_365_638_117_720_309_179M;
 
         #endregion
     }
