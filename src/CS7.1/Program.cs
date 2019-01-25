b33b436b88f1654339f115ced6998da11000e4a2
Emil Craciun <emil.craciun@live.com>
2019-01-25 11:59:24 +0200

    C# 7.1 complete

--- src/CS7.1/Program.cs
+++ src/CS7.1/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace CS7._1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await DoSomeAsyncStuff();
         }
+
+        private static async Task DoSomeAsyncStuff()
+        {
+            await Task.Delay(1000);
+            Console.WriteLine("Async stuff completed");
+        }
+
+        #region Default literal expressions
+
+        // before
+        Func<string, bool> whereClause = default(Func<string, bool>);
+
+        // after
+        Func<string, bool> whereClauseNew = default;
+
+        #endregion
+
+        #region Inferred tuple element names
+
+        private static void InferredTupleElementNames()
+        {
+            int count = 5;
+            string label = "Hello";
+            var pair = (count: count, label: label);
+
+            var newPair = (count, label); // element names are "count" and "label"
+            newPair.count = 10;
+        }
+
+
+        #endregion
     }
 }
